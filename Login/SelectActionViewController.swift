8ce32eb5e970db56672616e5f83b446822512d0c
Oschepkov Aleksandr <oao@i-cio.ru>
2021-10-10 10:25:48 +0300

    отдельно создал коллекцию фотографий (кнопка lesson 5) при загрузке главного экрана подгружаем фотографии с ВК, помещаем их в архив изображений, и передаем в DisplayPhotoCollection. Коллекцию строим уже из данных архива

--- Login/SelectActionViewController.swift
+++ Login/SelectActionViewController.swift
@@ -10,7 +10,30 @@ import UIKit
 class SelectActionViewController: UIViewController {
     var friends: [Friend] = []
     
+    //Блок работы с фотографиями
+    let photoAPI = PhotosAPI()
+    var photos: [Photo] = []
 
+    
+    func photoCache(_ photo: [Photo]) -> [UIImage]{
+        var photos: [Photo] = []
+        photos = photo
+        let e = photos.count
+        var imagesArray:[UIImage] = [UIImage]()
+        var i = 0
+        while i < e {
+            
+            let urlImage = URL(string:photos[i].sizes.first!.url)
+            if let data = try? Data(contentsOf: urlImage!) {
+                // Create Image and Update Image View
+                imagesArray.append(UIImage(data: data)!)
+            }
+            
+            i = i + 1
+        }
+        return(imagesArray)
+    }
+    
     @IBAction func actionsNewsFeed(_ sender: Any) {
         let newsVC = NewsWallController()
         self.navigationController?.pushViewController(newsVC, animated: true)
@@ -19,6 +42,10 @@ class SelectActionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        photoAPI.getPhotos {[weak self]  users in
+            self?.photos = users!
+          }
+        
         
     }
 // Очередь разбиата на несколько потоков парралельно, поэтому мы видим разный результат
@@ -35,6 +62,11 @@ class SelectActionViewController: UIViewController {
     @IBAction func actionGroupsPromise(_ sender: Any) {
         let GroupVC = GroupsViewController()
         self.navigationController?.pushViewController(GroupVC, animated: true)
+    }
+    @IBAction func actionPerf(_ sender: Any) {
+        let PhotoVC = DisplayPhotoViewController()
+        PhotoVC.photos = photoCache(photos)
+        self.navigationController?.pushViewController(PhotoVC, animated: true)
     }
 }
 
